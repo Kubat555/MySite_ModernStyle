0b52f67898bd97f46a6fea6563c363734aba351f
Kubat555 <kubat.chyngyzov@gmail.com>
2021-04-02 21:33:45 +0600

    test for getting text, price and image data

---
+++ scripts/add_cart_product.js
@@ -0,0 +1,12 @@
+let ttl = document.getElementsByClassName("product_name");
+console.log(ttl[0].innerText);
+
+let prise = document.getElementsByClassName("product_prise");
+console.log(prise[0].innerText);
+
+var img = document.getElementById('product_img'),
+  style = img.currentStyle || window.getComputedStyle(img, false),
+  bi = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+
+// Display the url to the user
+console.log('../img/product2.png ' + bi);
