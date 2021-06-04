bd160a9f5073173c700baeea067a251ec2958133
Kubat555 <kubat.chyngyzov@gmail.com>
2021-06-04 17:03:14 +0600

    add delete-function and total prise of carts

--- scripts/add_cart_product.js
+++ scripts/add_cart_product.js
@@ -14,31 +14,43 @@ btnbag.onclick = function(){
 };
 
 let element = document.getElementsByClassName("add_bag");
-var bag = document.getElementById("bag");
+var bag = document.getElementById("bag__carts");
+var tprise = document.getElementById("totalprise");
+var prisesum = 0;
 var getinfo = function(){
   var prdname = this.parentNode.parentNode.parentNode.querySelectorAll(".product_name");
   var prdprise = this.parentNode.parentNode.parentNode.querySelectorAll(".product_prise");
   var prdimg = this.parentNode.parentNode.parentNode.querySelectorAll(".imgproduct");
   const styles = window.getComputedStyle(prdimg[0]);
-  console.log(styles.backgroundImage);
 
   var cart = document.createElement("div");
   cart.className = "cart";
-  cart.innerHTML = `<img class="cart__img" src="./img/product2.png" alt=""><div class="cart__info"><p class="cart_name">${prdname[0].innerText}</p><p class="cart__prise">Цена: <span class="prise__num">${prdprise[0].innerText}</span> рублей</p><button id="bagbtn" class="btn lg-hvr btnbox bagbtn">удалить</button></div>`;
+  cart.innerHTML = `<img class="cart__img" src="./img/product2.png" alt=""><div class="cart__info"><p class="cart_name">${prdname[0].innerText}</p><p class="cart__prise">Цена: <span class="prise__num">${prdprise[0].innerText}</span> рублей</p><button  id="bagbtn" class="btn lg-hvr btnbox delbtn">удалить</button></div>`;
   bag.append(cart);
+
+  prisesum += +(prdprise[0].innerText);
+  tprise.innerHTML = prisesum;
+
+  clickbag();
 };
 
 for(var i = 0; i < element.length; i++){
   element[i].addEventListener('click', getinfo,false);
 }
 
-let deletebag = document.getElementsByClassName('bagbtn');
+
 var deleteinfo = function(){
-  var d = this.parentNode.parentNode;
-  console.log(d);
+  var delcart = this.parentNode.parentNode.remove();
+  var prdprise = this.parentNode.querySelectorAll(".prise__num");
+  prisesum -= +(prdprise[0].innerText);
+  tprise.innerHTML = prisesum;
+};
+
+function clickbag(){
+  var deltrash = document.querySelectorAll(".delbtn");
+  for(var i = 0; i < deltrash.length; i++){
+    deltrash[i].addEventListener('click', deleteinfo ,false);
+  }
 }
 
-for(var i = 0; i < btnbag.length; i++){
-  btnbag[i].addEventListener('click', deleteinfo,false);
-}
 
